d5b9157def0c5c9d961c8750a02f97c69f064a7b
Nicolas Bologna <nicolasandresbologna@hotmail.com>
2022-05-19 00:06:00 -0300

    Add project files.

---
+++ ClaseConstructores/Celular.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaseConstructores
+{
+    public class Celular
+    {
+        public string Nombre { get; set; } = string.Empty;
+
+        public Celular()
+        {
+            Console.WriteLine("Constructor Celular()");
+        }
+
+        public Celular(string nombre)
+        {
+            Console.WriteLine(nombre);
+        }
+
+
+        public virtual void SonarNoti()
+        {
+            //suena
+        }
+    }
+    
+    public class CelularLiberado : Celular
+    {
+        public override void SonarNoti()
+        {
+            //suena liberado
+        }
+
+        public CelularLiberado(string nombre) //esto va a llamar al constructor por defecto, osea a Celular(). Si borramos Celular() va a fallar, vamos a tener que definir que llame al otro constructor.
+        {
+            Console.WriteLine(nombre);
+        }
+        public CelularLiberado(string nombre, string marca) : base(nombre)
+        {
+            Console.WriteLine(marca);
+        }
+
+    }
+}
---
+++ ClaseConstructores/Program.cs
@@ -0,0 +1,8 @@
+﻿using ClaseConstructores;
+
+//var celular = new Celular();
+//var celular2 = new Celular("galaxy");
+
+var celularLiberado = new CelularLiberado("iphone");
+
+var celularLiberado2 = new CelularLiberado("iphone", "Apple");
